16543ff0a16822d76d1e89d50501513910dd292f
ooooonnnnn <79533443+ooooonnnnn@users.noreply.github.com>
2026-08-25 19:22:21 +0300

    .

--- Server/obj/Debug/net10.0/Trivia Game Server.AssemblyInfo.cs
+++ Server/obj/Debug/net10.0/Trivia Game Server.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Trivia Game Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9c19e96dfd76acdb0f1673b2e7d87d123bb474ae")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bdd31a4585aa93054ea837e30d5d5edf730e4bdd")]
 [assembly: System.Reflection.AssemblyProductAttribute("Trivia Game Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Trivia Game Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
