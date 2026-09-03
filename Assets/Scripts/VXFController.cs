7de2d0ec8a21336ea35765b013609a8fb3ed2c4d
ooooonnnnn <79533443+ooooonnnnn@users.noreply.github.com>
2026-09-04 00:43:37 +0300

    fix vfx

--- Assets/Scripts/VXFController.cs
+++ Assets/Scripts/VXFController.cs
@@ -6,12 +6,22 @@ public class VXFController : MonoBehaviour
     [SerializeField] private GameObject_Float_Pair correctEffect, wrongEffect;
 
     [ContextMenu("Show Correct Effect")]
-    public void ShowCorrectEffect() => StartCoroutine(
-        FlashObject(correctEffect.gameObject, correctEffect.number));
-    
+    public void ShowCorrectEffect()
+    {
+        StopAllCoroutines();
+        
+        StartCoroutine(
+            FlashObject(correctEffect.gameObject, correctEffect.number));
+    }
+
     [ContextMenu("Show Wrong Effect")]
-    public void ShowWrongEffect() => StartCoroutine(
-        FlashObject(wrongEffect.gameObject, wrongEffect.number));
+    public void ShowWrongEffect()
+    {
+        StopAllCoroutines();
+        
+        StartCoroutine(
+            FlashObject(wrongEffect.gameObject, wrongEffect.number));
+    }
 
     private IEnumerator FlashObject(GameObject obj, float duration)
     {
